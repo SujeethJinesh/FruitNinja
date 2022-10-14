66fe565c212fee31b34918538eb213b8c462fb0e
SujeethJinesh <sujeethjinesh@gmail.com>
2022-10-14 00:16:41 -0700

    Add Slicing Blade

---
+++ Assets/Scripts/Blade.cs
@@ -0,0 +1,72 @@
+using UnityEngine;
+
+public class Blade : MonoBehaviour
+{
+    private Camera mainCamera;
+    private bool slicing;
+    private Collider bladeCollider;
+
+    public Vector3 direction { get; private set; }
+    public float minSliceVelocity = 0.01f;
+
+    private void Awake()
+    {
+        bladeCollider = GetComponent<Collider>();
+        mainCamera = Camera.main;
+    }
+
+    private void OnEnable()
+    {
+        StopSlicing();
+    }
+
+    private void OnDisable()
+    {
+        StopSlicing();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartSlicing();
+        } else if (Input.GetMouseButtonUp(0))
+        {
+            StopSlicing();
+        } else if (slicing)
+        {
+            ContinueSlicing();
+        }
+    }
+
+    private void StartSlicing()
+    {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        transform.position = newPosition;
+
+        slicing = true;
+        bladeCollider.enabled = true;
+    }
+
+    private void StopSlicing()
+    {
+        slicing = false;
+        bladeCollider.enabled = false;
+
+    }
+
+    private void ContinueSlicing()
+    {
+        Vector3 newPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        newPosition.z = 0f;
+
+        direction = newPosition - transform.position; // direction blade is moving
+
+        float velocity = direction.magnitude / Time.deltaTime;
+        bladeCollider.enabled = velocity > minSliceVelocity;
+
+        transform.position = newPosition;
+    }
+}
